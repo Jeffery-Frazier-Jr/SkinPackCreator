1cf7b80d67e8fffdab0ab223f216fb0fc472be18
Jeffery-Frazier-Jr <jeffery.frazier.jr@gmail.com>
2024-09-16 20:49:57 -0400

    Pushed frames into a layout class

--- Frames.py
+++ Frames.py
@@ -3,71 +3,82 @@ from tkinter import Canvas, filedialog
 from PIL import Image, ImageTk
 import os
 
-def FileSelect(parent):
+class Layout(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(parent)
 
-    # grid
-    FileSelectFrame = ctk.CTkFrame(parent, fg_color = '#222222')
-    FileSelectFrame.rowconfigure((0,1,2,3,4), weight = 1)
-    FileSelectFrame.columnconfigure(0, weight = 2)
-    FileSelectFrame.columnconfigure(1, weight = 1)
+        self.settings_control = ctk.CTkFrame(parent)
+        self.folder_output = ctk.CTkFrame(parent)
 
-    # widgets
-    Pack_exportLabel = ctk.CTkLabel(FileSelectFrame, text = 'Pack Export\nName')
-    Pack_exportEntry = ctk.CTkEntry(FileSelectFrame)
-    Localization_ent = ctk.CTkEntry(FileSelectFrame)
-    Default_all_btn = ctk.CTkButton(FileSelectFrame, text = 'DEF all')
-    Skin_SelectLabel = ctk.CTkLabel(FileSelectFrame, text = 'Select Skin')
-    Skin_select_menu = ctk.CTkOptionMenu(FileSelectFrame, values = ['Select File'])
-    Cape_SelectLabel = ctk.CTkLabel(FileSelectFrame, text = 'Select Cape')
-    Cape_select_menu = ctk.CTkOptionMenu(FileSelectFrame, values = ['Select File'])
-    
-    # placement
-    Pack_exportLabel.grid(row = 0, column = 1, sticky = 'w', padx = 10)
-    Pack_exportEntry.grid(row = 0, column = 0, sticky = 'ew')
-    Localization_ent.grid(row = 2, column = 0, sticky = 'ew')
-    Default_all_btn.grid(row = 4, column = 0)
-    Skin_SelectLabel.grid(row = 1, column = 1, sticky = 's')
-    Skin_select_menu.grid(row = 2, column = 1, sticky = 'n')
-    Cape_SelectLabel.grid(row = 3, column = 1, sticky = 's')
-    Cape_select_menu.grid(row = 4, column = 1, sticky = 'n')
-    FileSelectFrame.place(relx = 0, rely = 0, relwidth = 1, relheight = .5)
+        self.FileSelect()
+        self.ExportControls()
+        self.FileOutput()
 
-def ExportControls(parent):
-    
-    # grid
-    ExportControlsFrame = ctk.CTkFrame(parent, fg_color = '#191919')
-    ExportControlsFrame.rowconfigure((0,1,2), weight = 1)
-    ExportControlsFrame.columnconfigure((0,1), weight = 1)
-    
-    # widgets
-    OutputLabel = ctk.CTkLabel(ExportControlsFrame, text = 'Output Label')
-    AddButton = ctk.CTkButton(ExportControlsFrame, text = 'Add', fg_color = '#10b409', hover_color = '#077d02')
-    ExportButton = ctk.CTkButton(ExportControlsFrame, text = 'Export', fg_color = '#c61d1d', hover_color = '#830d0d')
-    Export_PATHEntry = ctk.CTkEntry(ExportControlsFrame)
-    
-    # placement
-    OutputLabel.grid(row = 0, column = 0, columnspan = 2)
-    AddButton.grid(row = 1, column = 0)
-    ExportButton.grid(row = 1, column = 1)
-    Export_PATHEntry.grid(row = 2, column = 0, columnspan = 2)
-    ExportControlsFrame.place(relx = 0, rely = .5, relwidth = 1, relheight = .5)
+        self.settings_control.place(relx = 0, rely = 0, relwidth = .4, relheight = 1)
+        self.folder_output.place(relx = .4, rely = 0, relwidth = .6, relheight = 1)
 
-def FileOutput(parent):
+    def FileSelect(self):
+        # grid
+        FileSelectFrame = ctk.CTkFrame(self.settings_control, fg_color = '#222222')
+        FileSelectFrame.rowconfigure((0,1,2,3,4), weight = 1)
+        FileSelectFrame.columnconfigure(0, weight = 2)
+        FileSelectFrame.columnconfigure(1, weight = 1)
 
-    # grid
-    FileOutputFrame = ctk.CTkFrame(parent)
-    FileOutputFrame.rowconfigure((0,1), weight = 1, uniform = 'a')
-    FileOutputFrame.columnconfigure((0,1), weight = 1, uniform = 'a')
-    
-    # widgets
-    Current_SkinLabel = ctk.CTkLabel(FileOutputFrame, text = 'Current\nSkin', fg_color = '#292929', text_color = 'white')
-    Current_CapeLabel = ctk.CTkLabel(FileOutputFrame, text = 'Current\nCape', fg_color = '#292929', text_color = 'white')
-    Skin_Folder_SelectButton = ctk.CTkButton(FileOutputFrame, text = 'Skin\nFolder\nSelect', fg_color = '#d35e0a', hover_color = '#934005')
-    Cape_Folder_SelectButton = ctk.CTkButton(FileOutputFrame, text = 'Cape\nFolder\nSelect', fg_color = '#d35e0a', hover_color = '#934005')
-    
-    # placement
-    Current_SkinLabel.grid(row = 0, column = 0, sticky = 'news', padx = 10, pady = 10)
-    Current_CapeLabel.grid(row = 1, column = 0, sticky = 'news', padx = 10, pady = 10)
-    Skin_Folder_SelectButton.grid(row = 0, column = 1, sticky = 'news', padx = 10, pady = 10)
-    Cape_Folder_SelectButton.grid(row = 1, column = 1, sticky = 'news', padx = 10, pady = 10)
-    FileOutputFrame.place(relx = 0, rely = 0, relwidth = 1, relheight = 1)
+        # widgets
+        Pack_exportLabel = ctk.CTkLabel(FileSelectFrame, text = 'Pack Export\nName')
+        Pack_exportEntry = ctk.CTkEntry(FileSelectFrame)
+        Localization_ent = ctk.CTkEntry(FileSelectFrame)
+        Default_all_btn = ctk.CTkButton(FileSelectFrame, text = 'DEF all')
+        Skin_SelectLabel = ctk.CTkLabel(FileSelectFrame, text = 'Select Skin')
+        Skin_select_menu = ctk.CTkOptionMenu(FileSelectFrame, values = ['Select File'])
+        Cape_SelectLabel = ctk.CTkLabel(FileSelectFrame, text = 'Select Cape')
+        Cape_select_menu = ctk.CTkOptionMenu(FileSelectFrame, values = ['Select File'])
+
+        # placement
+        Pack_exportLabel.grid(row = 0, column = 1, sticky = 'w', padx = 10)
+        Pack_exportEntry.grid(row = 0, column = 0, sticky = 'ew')
+        Localization_ent.grid(row = 2, column = 0, sticky = 'ew')
+        Default_all_btn.grid(row = 4, column = 0)
+        Skin_SelectLabel.grid(row = 1, column = 1, sticky = 's')
+        Skin_select_menu.grid(row = 2, column = 1, sticky = 'n')
+        Cape_SelectLabel.grid(row = 3, column = 1, sticky = 's')
+        Cape_select_menu.grid(row = 4, column = 1, sticky = 'n')
+        FileSelectFrame.place(relx = 0, rely = 0, relwidth = 1, relheight = .5)
+
+    def ExportControls(self):
+        # grid
+        ExportControlsFrame = ctk.CTkFrame(self.settings_control, fg_color = '#191919')
+        ExportControlsFrame.rowconfigure((0,1,2), weight = 1)
+        ExportControlsFrame.columnconfigure((0,1), weight = 1)
+
+        # widgets
+        OutputLabel = ctk.CTkLabel(ExportControlsFrame, text = 'Output Label')
+        AddButton = ctk.CTkButton(ExportControlsFrame, text = 'Add', fg_color = '#10b409', hover_color = '#077d02')
+        ExportButton = ctk.CTkButton(ExportControlsFrame, text = 'Export', fg_color = '#c61d1d', hover_color = '#830d0d')
+        Export_PATHEntry = ctk.CTkEntry(ExportControlsFrame)
+
+        # placement
+        OutputLabel.grid(row = 0, column = 0, columnspan = 2)
+        AddButton.grid(row = 1, column = 0)
+        ExportButton.grid(row = 1, column = 1)
+        Export_PATHEntry.grid(row = 2, column = 0, columnspan = 2)
+        ExportControlsFrame.place(relx = 0, rely = .5, relwidth = 1, relheight = .5)
+
+    def FileOutput(self):
+        # grid
+        FileOutputFrame = ctk.CTkFrame(self.folder_output)
+        FileOutputFrame.rowconfigure((0,1), weight = 1, uniform = 'a')
+        FileOutputFrame.columnconfigure((0,1), weight = 1, uniform = 'a')
+
+        # widgets
+        Current_SkinLabel = ctk.CTkLabel(FileOutputFrame, text = 'Current\nSkin', fg_color = '#292929', text_color = 'white')
+        Current_CapeLabel = ctk.CTkLabel(FileOutputFrame, text = 'Current\nCape', fg_color = '#292929', text_color = 'white')
+        Skin_Folder_SelectButton = ctk.CTkButton(FileOutputFrame, text = 'Skin\nFolder\nSelect', fg_color = '#d35e0a', hover_color = '#934005')
+        Cape_Folder_SelectButton = ctk.CTkButton(FileOutputFrame, text = 'Cape\nFolder\nSelect', fg_color = '#d35e0a', hover_color = '#934005')
+
+        # placement
+        Current_SkinLabel.grid(row = 0, column = 0, sticky = 'news', padx = 10, pady = 10)
+        Current_CapeLabel.grid(row = 1, column = 0, sticky = 'news', padx = 10, pady = 10)
+        Skin_Folder_SelectButton.grid(row = 0, column = 1, sticky = 'news', padx = 10, pady = 10)
+        Cape_Folder_SelectButton.grid(row = 1, column = 1, sticky = 'news', padx = 10, pady = 10)
+        FileOutputFrame.place(relx = 0, rely = 0, relwidth = 1, relheight = 1)
--- SkinPackCreator.py
+++ SkinPackCreator.py
@@ -10,16 +10,17 @@ class App(ctk.CTk):
         self.title('SkinPackCreator')
         ctk.set_appearance_mode('dark')
 
+        Layout(self)
         # main layout
-        settings_control = Frame(self, 0, 0, .4, 1)
-        folder_output = Frame(self, .4, 0, .6, 1)
+        # settings_control = Frame(self, 0, 0, .4, 1)
+        # folder_output = Frame(self, .4, 0, .6, 1)
 
         # settings layout
         # file_selection = Frame(settings_control, 0, 0, 1, .5, 'white')       # panels
-        FileSelect(settings_control)
+        # FileSelect(settings_control)
         
         # export_controls = Frame(settings_control, 0, .5, 1, .5, 'pink')
-        ExportControls(settings_control)
+        # ExportControls(settings_control)
         
         # export_controls.rowconfigure((0,1,2), weight = 1, uniform = 'b')
         # export_controls.columnconfigure((0,1), weight = 1, uniform = 'b')
@@ -32,7 +33,7 @@ class App(ctk.CTk):
         # DropMenu(file_selection, 2, 1, ['More', 'or', 'less', 'know'])
 
         # File view & Folder select frame
-        FileOutput(folder_output)
+        # FileOutput(folder_output)
         # folder_output.rowconfigure((0,1), weight = 1, uniform = 'a')         # grid
         # folder_output.columnconfigure((0,1), weight = 1, uniform = 'a')
         # 
